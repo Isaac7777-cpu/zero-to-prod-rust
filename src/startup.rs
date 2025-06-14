7e4b95aacc95ad02f2333f8364bb4fd6dea60750
Isaac Leong <isaacleong23367@gmail.com>
2025-06-14 23:30:22 +1000

    Finished Most Logging

--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,21 @@
-use std::net::TcpListener;
-
+use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
+use sqlx::PgPool;
+use std::net::TcpListener;
 
-use crate::routes::{health_check, subscribe};
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
+    // Capture `connection` from teh surrounding environment
+    let server = HttpServer::new(move || {
         App::new()
+            // Added middlewares are using the `wrap` method on `App`
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            // Get a pointer copy and attach it to the application state
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
