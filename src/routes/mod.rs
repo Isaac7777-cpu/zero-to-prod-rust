503d0901459a2cd54bfc97f5425992c4c061d8f2
Isaac Leong <isaacleong23367@gmail.com>
2025-07-03 18:17:00 +0800

    Hot Fix

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,6 +2,7 @@ mod admin;
 mod health_check;
 mod home;
 mod login;
+mod newsletters;
 mod subscriptions;
 mod subscriptions_confirm;
 
@@ -9,5 +10,6 @@ pub use admin::*;
 pub use health_check::*;
 pub use home::*;
 pub use login::*;
+pub use newsletters::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
