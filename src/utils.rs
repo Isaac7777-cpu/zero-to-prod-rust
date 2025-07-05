64eef6cf946a33a8c24a1f8c851eb0fe48d466dd
Isaac Leong <isaacleong23367@gmail.com>
2025-07-05 16:35:04 +0800

    Updated for Non-Concurrent Idempotent

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,12 @@
 use actix_web::http::header::LOCATION;
 use actix_web::HttpResponse;
 
+pub fn see_other(location: &str) -> HttpResponse {
+    HttpResponse::SeeOther()
+        .insert_header((LOCATION, location))
+        .finish()
+}
+
 pub fn e500<T>(e: T) -> actix_web::Error
 where
     T: std::fmt::Debug + std::fmt::Display + 'static,
@@ -8,8 +14,9 @@ where
     actix_web::error::ErrorInternalServerError(e)
 }
 
-pub fn see_other(location: &str) -> HttpResponse {
-    HttpResponse::SeeOther()
-        .insert_header((LOCATION, location))
-        .finish()
+pub fn e400<T>(e: T) -> actix_web::Error
+where
+    T: std::fmt::Debug + std::fmt::Display + 'static,
+{
+    actix_web::error::ErrorBadRequest(e)
 }
